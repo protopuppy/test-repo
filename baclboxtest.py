d557fb20af55b69ad6b820c5031f562253368cad
protopuppy <92961116+protopuppy@users.noreply.github.com>
2022-10-02 14:53:02 +0530

    Create baclboxtest.py

---
+++ baclboxtest.py
@@ -0,0 +1,14 @@
+  SH: dnsdbq -r
+  ("input4.yaml", line 6)
+* new test output
+  dnsdbq: option requires an argument -- r
+  error: unrecognized option
+  usage: dnsdbq [-vdjsShc] [-p dns|json|csv] [-k (first|last|count)[,...]]
+[etc]
+
+> Press ENTER to record, 's'+ENTER to skip, 'h'+ENTER to halt
+> 
+> Press ENTER to save changes, 'd'+ENTER to discard changes
+> 
+*saving test output to 'output4.yaml'
+===========================================
